481c670ef326302fb151c24fc9ada79290763758
Young <younghotkim@gmail.com>
2021-08-06 09:56:17 +0900

    mystie05 / 0806 / fileup

--- src/main/java/com/javaex/api/controller/ApiGalleryController.java
+++ src/main/java/com/javaex/api/controller/ApiGalleryController.java
@@ -1,5 +1,10 @@
 package com.javaex.api.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/api/gallery")
 public class ApiGalleryController {
 
 }
